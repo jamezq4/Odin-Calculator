e019b3597d694ce5cdd40a3c8b6441ca91a1f6f4
jamezq <jamezquita240@gmail.com>
2023-02-15 08:52:55 -0600

    Define all global variables

---
+++ script.js
@@ -0,0 +1,12 @@
+//array to store current number of pair
+let currentNumber = [];
+currentNumber[0] = '0';
+
+//array to store total numbers to calculate
+let calculateNumbers = [];
+calculateNumbers[0] = 0;
+
+
+//store operator and calculcation total for the current pair
+let currentOperator = '';
+let currentTotal;
